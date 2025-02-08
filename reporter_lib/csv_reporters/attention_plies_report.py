8e428060b2e2c45e93a5c834e29b843e9235851e
alireza <alireza.infotech@gmail.com>
2025-02-08 15:30:50 +0100

    feat: adding attention plies report

---
+++ reporter_lib/csv_reporters/attention_plies_report.py
@@ -0,0 +1,42 @@
+from logging import Logger
+
+from reporter_lib.csv_reporters.reporter_abstraction import ReporterAbstract
+
+
+class CSVAttentionPliesFormatter(ReporterAbstract):
+    _fields = ["file", "ply", "cell", "ee", "success_rate"]
+
+    def __init__(
+        self,
+        display_name: str,
+        display_description: str,
+        input_file: str,
+        logger: Logger,
+    ) -> None:
+        super().__init__(
+            display_name=display_name,
+            display_description=display_description,
+            input_file=input_file,
+            _output_file_name_postfix="_attention",
+            logger=logger,
+        )
+
+    async def _get_sorted_ply_results(self):
+        return sorted(
+            self._data.ply_results, key=lambda p: p.success_rate, reverse=True
+        )
+
+    async def _process_data(self) -> None:
+        ply_results = await self._get_sorted_ply_results()
+
+        for ply in ply_results:
+            if ply.success_rate < 1:
+                self._rows.append(
+                    [
+                        ply.picks[0].plyshape.parent_file,
+                        ply.picks[0].plyshape.label,
+                        ply.picks[0].cell_label,
+                        ply.picks[0].end_effector_label,
+                        round(100 * ply.success_rate),
+                    ]
+                )
